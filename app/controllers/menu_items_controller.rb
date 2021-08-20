7263e703f81a2e8b1477734533e29fb891609d4e
Preethi-kancharla <preethi.kancharla@freshworks.com>
2021-08-20 20:11:16 +0530

    added edit update destroy methods

--- app/controllers/menu_items_controller.rb
+++ app/controllers/menu_items_controller.rb
@@ -31,4 +31,32 @@ class MenuItemsController < ApplicationController
         end
     end
 
+    def edit
+      @menu_item = MenuItem.find(params[:id])
+      menuid=@menu_item.menu_id
+      @menu=Menu.find(menuid)
+    end
+  
+    def update
+      menu = Menu.check_menu_exists_create_if_not(params[:menu_name])
+      menu.save
+      menu_item = MenuItem.find(params[:id])
+      menu_item.update(name: params[:name].capitalize,
+                       description: params[:description].capitalize,
+                       menu_id: menu.id,
+                       price: params[:price])
+      if menu.save && menu_item.save
+        redirect_to(menus_path, notice: "#{menu_item.name} item updated successfully!")
+      else
+        flash[:error] = menu_item.errors.full_messages + menu.errors.full_messages
+        redirect_to edit_menu_item_path
+      end
+    end
+
+    def destroy
+      @menu_item = MenuItem.find(params[:id])
+      @menu_item.destroy!
+      edirect_to menu_items_path
+    end
+
 end
--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -7,9 +7,33 @@ class MenusController < ApplicationController
       render plain: Menu.find(params[:id]).name
     end
 
+    def new
+    end
+
+    def edit
+        @menu = Menu.find(params[:id])
+    end
+
+    def update
+        menu = Menu.find(params[:id])
+        puts "params #{params[:name]}"
+        menu.update(name: params[:name].capitalize)
+        if menu.save
+          redirect_to(menus_path, notice: "#{menu.name} menu updated successfully!")
+        else
+          flash[:error] = menu.errors.full_messages
+          redirect_to edit_menu_path
+        end
+    end
+
     def create
         menu = Menu.create!(name: params[:name].capitalize)
         render plain: "created #{menu.name} with #{menu.id}"
     end
 
+    def destroy
+        @menu = Menu.find(params[:id])
+        @menu.destroy!
+        redirect_to menus_path
+    end
 end
