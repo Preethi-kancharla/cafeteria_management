773aad88bf835373c66ae0eb53398036ab3748f2
Preethi-kancharla <preethi.kancharla@freshworks.com>
2021-08-20 12:44:44 +0530

    Displayed menus and menuitems pages by creating data in rails console

--- app/controllers/menu_items_controller.rb
+++ app/controllers/menu_items_controller.rb
@@ -1,2 +1,9 @@
 class MenuItemsController < ApplicationController
+    def index
+        render plain: MenuItem.all.to_a.map { |item| item.name }.join("\n")
+    end
+
+    def show
+        render plain: MenuItem.find(params[:id]).name
+    end
 end
--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,2 +1,9 @@
 class MenusController < ApplicationController
+    def index
+        render plain: Menu.all.to_a.map { |menu| menu.name }.join("\n")
+    end
+    
+    def show
+      render plain: Menu.find(params[:id]).name
+    end
 end
